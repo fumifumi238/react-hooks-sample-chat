4acbfbf25d83e9d78cb0a55f2c3c851dcdee59c2
epthalite <fmy628.1998@icloud.com>
2022-07-29 09:22:24 +0900

    sever explain

--- chat-server/app.js
+++ chat-server/app.js
@@ -4,8 +4,11 @@ const io = require('socket.io')(server);
 
 io.on('connection', (socket) => {
     console.log('A client connected.');
+
+    // 送られたメッセージを受け取る
     socket.on('send', (payload) => {
         console.log(payload);
+        // sendしてきた人以外にメッセージを送る
         socket.broadcast.emit('broadcast', payload);
     });
     socket.on('disconnect', () => {
